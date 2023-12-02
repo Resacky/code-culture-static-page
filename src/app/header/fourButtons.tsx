a78012d9164a37a244b5c91e357bb9a8aa0a4058
Joseph Ruiz <Ruiz-joseph@hotmail.com>
2023-12-01 19:50:23 -0500

    more CSS logic
    Added more CSS logic for the four header buttons

--- src/app/header/fourButtons.tsx
+++ src/app/header/fourButtons.tsx
@@ -1,26 +1,42 @@
+'use client';
 import styles from './styles.module.css';
 import Link from 'next/link';
 import Image from 'next/image';
+import React, { useState } from 'react';
 
 import homepage from '../../assets/homepage.svg'
 import play from '../../assets/media.svg'
 import code from '../../assets/code.svg'
 import group from '../../assets/group.svg'
+import greenHomepage from '../../assets/green_homepage.svg'
+import greenPlay from '../../assets/green_play-button.svg'
+import greenCode from '../../assets/green_code.svg'
+import greenGroup from '../../assets/green_group.svg'
 
 export default function FourButtons() {
+    const [isHomepageClicked, setHomepageClicked] = useState(false);
+    const [isPlayClicked, setPlayClicked] = useState(false);
+    const [isCodeClicked, setCodeClicked] = useState(false);
+    const [isGroupClicked, setGroupClicked] = useState(false);
+
+    const handleHomepageClick = () => setHomepageClicked(!isHomepageClicked);
+    const handlePlayClick = () => setPlayClicked(!isPlayClicked);
+    const handleCodeClick = () => setCodeClicked(!isCodeClicked);
+    const handleGroupClick = () => setGroupClicked(!isGroupClicked);
+
     return (
         <div className={styles.fourButtons}>
             <Link href="/">
-                <Image className={styles.homepage} src={homepage} alt="" draggable="false" />
+                <Image className={`${styles.homepage} ${isHomepageClicked ? styles.imageClicked : ''}`}  src={isHomepageClicked ? greenHomepage : homepage} alt="" draggable="false" onClick={handleHomepageClick} />
             </Link>
             <Link href="/">
-                <Image className={styles.play} src={play} alt="" draggable="false" />
+                <Image className={`${styles.play} ${isPlayClicked ? styles.imageClicked : ''}`}  src={isPlayClicked ? greenPlay : play} alt="" draggable="false" onClick={handlePlayClick} />
             </Link>
             <Link href="/">
-                <Image className={styles.code} src={code} alt="" draggable="false" />
+                <Image className={`${styles.code} ${isCodeClicked ? styles.imageClicked : ''}`} src={isCodeClicked ? greenCode : code} alt="" draggable="false" onClick={handleCodeClick} />
             </Link>
             <Link href="/">
-                <Image className={styles.group} src={group} alt="" draggable="false" />
+                <Image className={`${styles.group} ${isGroupClicked ? styles.imageClicked : ''}`} src={isGroupClicked ? greenGroup : group} alt="" draggable="false" onClick={handleGroupClick} />
             </Link>
         </div>
     );

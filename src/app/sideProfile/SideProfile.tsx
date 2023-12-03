ec5ed1d7bd30bae66dbb5c3f7f6a1cacdc9ce1a6
Joseph Ruiz <Ruiz-joseph@hotmail.com>
2023-12-03 18:05:00 -0500

    added more responsiveness

--- src/app/sideProfile/SideProfile.tsx
+++ src/app/sideProfile/SideProfile.tsx
@@ -14,54 +14,59 @@ import Link from 'next/link';
 export default function SideProfile() {
     return (
         <div className={styles.sideProfile}>
-            <Link className={styles.container} href="/" >
-                <div className={styles.profile}>
-                    <Image src={profile} alt="" draggable="false" className={styles.profileImage} />
-                </div>
-                <p className={styles.text}> Profile </p>
-            </Link>
-            <Link className={styles.container} href="/" >
-                <div className={styles.groupProfile}>
-                    <Image src={groupColored} alt="" draggable="false" className={styles.groupImage} />
-                </div>
-                <p className={styles.text}> Coder Companion </p>
-            </Link>
-            <Link className={styles.container} href="/" >
-                <div className={styles.groupProfile}>
-                    <Image src={post} alt="" draggable="false" className={styles.groupImage} />
-                </div>
-                <p className={styles.text}> Post Something </p>
-            </Link>
-            <Link className={styles.container} href="/" >
-                <div className={styles.groupProfile}>
-                    <Image src={question} alt="" draggable="false" className={styles.groupImage} />
-                </div>
-                <p className={styles.text}> Questions </p>
-            </Link>
-            <Link className={styles.container} href="/" >
-                <div className={styles.groupProfile}>
-                    <Image src={media} alt="" draggable="false" className={styles.mediaImage} />
-                </div>
-                <p className={styles.text}> Media </p>
-            </Link>
-            <Link className={styles.container} href="/" >
-                <div className={styles.groupProfile}>
-                    <Image src={events} alt="" draggable="false" className={styles.groupImage} />
-                </div>
-                <p className={styles.text}> Events </p>
-            </Link>
-            <Link className={styles.container} href="/" >
-                <div className={styles.groupProfile}>
-                    <Image src={bookmark} alt="" draggable="false" className={styles.groupImage} />
-                </div>
-                <p className={styles.text}> Bookmarks </p>
-            </Link>
-            <Link className={styles.container} href="/" >
-                <div className={styles.groupProfile}>
-                    <Image src={logo} alt="" draggable="false" className={styles.logo} />
-                </div>
-                <p className={styles.text}> Premium </p>
-            </Link>
+            <div className={styles.leftGrid}>
+                <Link className={styles.container} href="/" >
+                    <div className={styles.profile}>
+                        <Image src={profile} alt="" draggable="false" className={styles.profileImage} />
+                    </div>
+                    <p className={styles.text}> Profile </p>
+                </Link>
+                <Link className={styles.container} href="/" >
+                    <div className={styles.groupProfile}>
+                        <Image src={groupColored} alt="" draggable="false" className={styles.groupImage} />
+                    </div>
+                    <p className={styles.text}> Coder Companion </p>
+                </Link>
+                <Link className={styles.container} href="/" >
+                    <div className={styles.groupProfile}>
+                        <Image src={post} alt="" draggable="false" className={styles.groupImage} />
+                    </div>
+                    <p className={styles.text}> Post Something </p>
+                </Link>
+                <Link className={styles.container} href="/" >
+                    <div className={styles.groupProfile}>
+                        <Image src={question} alt="" draggable="false" className={styles.groupImage} />
+                    </div>
+                    <p className={styles.text}> Questions </p>
+                </Link>
+                <Link className={styles.container} href="/" >
+                    <div className={styles.groupProfile}>
+                        <Image src={media} alt="" draggable="false" className={styles.mediaImage} />
+                    </div>
+                    <p className={styles.text}> Media </p>
+                </Link>
+                <Link className={styles.container} href="/" >
+                    <div className={styles.groupProfile}>
+                        <Image src={events} alt="" draggable="false" className={styles.groupImage} />
+                    </div>
+                    <p className={styles.text}> Events </p>
+                </Link>
+                <Link className={styles.container} href="/" >
+                    <div className={styles.groupProfile}>
+                        <Image src={bookmark} alt="" draggable="false" className={styles.groupImage} />
+                    </div>
+                    <p className={styles.text}> Bookmarks </p>
+                </Link>
+                <Link className={styles.container} href="/" >
+                    <div className={styles.groupProfile}>
+                        <Image src={logo} alt="" draggable="false" className={styles.logo} />
+                    </div>
+                    <p className={styles.text}> Premium </p>
+                </Link>
+            </div>
+            <div className={styles.rightGrid}>
+                <Image className={styles.profile} src={profile} alt={''} />
+            </div>
         </div>
     );
 }
